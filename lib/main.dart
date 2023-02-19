d0f40e109ce8549bfb06b1de5bd7f6e2c5937022
imminh20x <imminh20x@gmail.com>
2023-02-19 16:22:35 +0700

    get location from geolocator api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/loading_screen.dart';
 
 void main(List<String> args) {
-  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: LoadingScreen(),
+    );
+  }
 }
